0693ccf974c4c9edadc7e07f886a56b3e393fb94
Pavece <55211797+pavece@users.noreply.github.com>
2025-06-14 12:25:26 +0200

    back - feat: basic MCP tools for data retrieval

--- backend/internal/mcp/mcp.go
+++ backend/internal/mcp/mcp.go
@@ -1,7 +1,6 @@
 package mcp
 
 import (
-	"context"
 	"fmt"
 	"log"
 
@@ -14,9 +13,31 @@ func StartMCPServer(port string) {
         server.WithToolCapabilities(true),
     )
 	
-	s.AddTool(mcp.NewTool("hello_mcp", mcp.WithDescription("Get greetings from the MCP server")), func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-		return mcp.NewToolResultText("Hello MCP"), nil
-	})
+	// s.AddTool(mcp.NewTool("hello_mcp", mcp.WithDescription("Get greetings from the MCP server")), func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	// 	return mcp.NewToolResultText("Hello MCP"), nil
+	// })
+
+	tools := NewTools()
+
+	//Webhook tools
+	s.AddTool(mcp.NewTool("get-webhooks", 
+		mcp.WithDescription("Get a list of all the available webhooks in the system with reduced information")), 
+	tools.getWebhooks)
+	
+	s.AddTool(mcp.NewTool("get-webhook", 
+		mcp.WithDescription("Get detailed information from a webhook by specifying the id"), 
+		mcp.WithString("webhookId", mcp.Required(), mcp.Description("ID from the webhook you want to retrieve"))),
+	tools.getWebhookById)
+
+	//Event tools
+	s.AddTool(mcp.NewTool("get-events", 
+		mcp.WithDescription("Get the history of fired events registered in the system")), 
+	tools.getEvents)
+
+	s.AddTool(mcp.NewTool("get-events-by-webhook", 
+		mcp.WithDescription("Get all the events created by firing a webhook"), 
+		mcp.WithString("webhookId", mcp.Required(), mcp.Description("ID from the webhook you want to retrieve events from"))),
+	tools.getEventsByWebhook)
 
 
     fmt.Println("[INFO] Starting MCP server on " + port)
